c30ed5f61feb7447986958fda2ca5682fcb44f50
JN-Jones <jones_h1@web.de>
2012-11-04 20:05:54 +0100

    ACP Modul hinzugefügt

--- admin/modules/forum/newsletter.php
+++ admin/modules/forum/newsletter.php
@@ -7,9 +7,322 @@ if(!defined("IN_MYBB"))
 
 $page->add_breadcrumb_item($lang->newsletter, "index.php?module=forum-newsletter");
 
-$page->output_header($lang->newsletter);
+if($mybb->input['action'] == "do_add") {
+	if(!verify_post_check($mybb->input['my_post_key']))
+	{
+		$errors[] = $lang->invalid_post_verify_key2;
+	}
 
-echo "OK";
+    if(!strlen(trim($mybb->input['override_receive'])))
+	{
+		$errors[] = $lang->override_reveive_not;
+	}
+	if(!strlen(trim($mybb->input['subject'])))
+	{
+		$errors[] = $lang->subject_not;
+	}
+	if(!strlen(trim($mybb->input['plain'])))
+	{
+		$errors[] = $lang->plain_not;
+	}
+	if(!strlen(trim($mybb->input['html'])))
+	{
+		$mybb->input['html'] = "";
+	}
+	if(!strlen(trim($mybb->input['send'])))
+	{
+		$errors[] = $lang->send_not;
+	}
+
+	if(!isset($errors)) {
+		$sent = 0;
+		if($mybb->input['send'])
+		    $sent = TIME_NOW;
+
+		$insert = array(
+			"subject" => $db->escape_string($mybb->input['subject']),
+			"html" => $db->escape_string($mybb->input['html']),
+			"plain" => $db->escape_string($mybb->input['plain']),
+			"override_receive" => $db->escape_string($mybb->input['override_receive']),
+			"created" => TIME_NOW,
+			"sent" => $sent
+		);
+		$db->insert_query("newsletter", $insert);
+
+		flash_message($lang->newsletter_added, 'success');
+		admin_redirect("index.php?module=forum-newsletter");
+	} else
+		$mybb->input['action'] = "add";
+}
+if($mybb->input['action'] == "add") {
+	$page->add_breadcrumb_item($lang->add, "index.php?module=forum-newsletter&action=add");
+	$page->output_header($lang->add);
+	generate_tabs("add");
+	
+	if(isset($errors)) {
+		$page->output_inline_error($errors);
+		$override_receive = $mybb->input['override_receive'];
+		$subject = $mybb->input['subject'];
+		$html = $mybb->input['html'];
+		$plain = $mybb->input['plain'];
+		$send = $mybb->input['send'];
+	} else {
+		$override_receive = 0;
+		$subject = "";
+		$html = "";
+		$plain = "";
+		$send = 0;
+	}
+
+
+	$form = new Form("index.php?module=forum-newsletter&amp;action=do_add", "post");
+	$form_container = new FormContainer($lang->add);
+
+	$fid = "override_receive";
+	$override_receive = $form->generate_yes_no_radio($fid, $override_receive, true, array("id" => $fid."_yes", "class" => $fid), array("id" => $fid."_no", "class" => $fid));
+	$form_container->output_row($lang->override_receive." <em>*</em>", $lang->override_receive_desc, $override_receive);
+
+	$subject = $form->generate_text_box("subject", $subject);
+	$form_container->output_row($lang->subject." <em>*</em>", $lang->subject_desc, $subject);
+
+	$plain = $form->generate_text_area("plain", $plain);
+	$form_container->output_row($lang->plain." <em>*</em>", $lang->plain_desc, $plain);
+
+	$html = $form->generate_text_area("html", $html);
+	$form_container->output_row($lang->html, $lang->html_desc, $html);
+
+	$fid = "send";
+	$send = $form->generate_yes_no_radio($fid, $send, true, array("id" => $fid."_yes", "class" => $fid), array("id" => $fid."_no", "class" => $fid));
+	$form_container->output_row($lang->send." <em>*</em>", $lang->send_desc, $send);
+
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button($lang->submit);
+	$buttons[] = $form->generate_reset_button($lang->reset);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+}
+if($mybb->input['action'] == "do_edit") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message($lang->newsletter_no_id, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+	$id=(int)$mybb->input['id'];
+	$query = $db->simple_select("newsletter", "sent", "id='{$id}'");
+	if($db->num_rows($query) != 1)
+	{
+		flash_message($lang->newsletter_wrong_id, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+	$nl = $db->fetch_array($query);
+
+	if($nl['sent'] != 0)
+	{
+		flash_message($lang->newsletter_already_sent, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+
+   	if(!verify_post_check($mybb->input['my_post_key']))
+	{
+		$errors[] = $lang->invalid_post_verify_key2;
+	}
+
+    if(!strlen(trim($mybb->input['override_receive'])))
+	{
+		$errors[] = $lang->override_reveive_not;
+	}
+	if(!strlen(trim($mybb->input['subject'])))
+	{
+		$errors[] = $lang->subject_not;
+	}
+	if(!strlen(trim($mybb->input['plain'])))
+	{
+		$errors[] = $lang->plain_not;
+	}
+	if(!strlen(trim($mybb->input['html'])))
+	{
+		$mybb->input['html'] = "";
+	}
+	if(!strlen(trim($mybb->input['send'])))
+	{
+		$errors[] = $lang->send_not;
+	}
+
+	if(!isset($errors)) {
+		$sent = 0;
+		if($mybb->input['send'])
+		    $sent = TIME_NOW;
+
+		$update = array(
+			"subject" => $db->escape_string($mybb->input['subject']),
+			"html" => $db->escape_string($mybb->input['html']),
+			"plain" => $db->escape_string($mybb->input['plain']),
+			"override_receive" => $db->escape_string($mybb->input['override_receive']),
+			"sent" => $sent
+		);
+		$db->update_query("newsletter", $update, "id='{$id}'");
+
+		flash_message($lang->newsletter_edited, 'success');
+		admin_redirect("index.php?module=forum-newsletter");
+	} else
+		$mybb->input['action'] = "edit";
+}
+if($mybb->input['action'] == "edit") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message($lang->newsletter_no_id, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+	$id=(int)$mybb->input['id'];
+	$query = $db->simple_select("newsletter", "*", "id='{$id}'");
+	if($db->num_rows($query) != 1)
+	{
+		flash_message($lang->newsletter_wrong_id, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+	$nl = $db->fetch_array($query);
+
+	if($nl['sent'] != 0)
+	{
+		flash_message($lang->newsletter_already_sent, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+
+	$page->add_breadcrumb_item($lang->edit, "index.php?module=forum-newsletter&action=edit&id={$id}");
+	$page->output_header($lang->edit);
+	generate_tabs("edit");
+
+	if(isset($errors)) {
+		$page->output_inline_error($errors);
+		$override_receive = $mybb->input['override_receive'];
+		$subject = $mybb->input['subject'];
+		$html = $mybb->input['html'];
+		$plain = $mybb->input['plain'];
+		$send = $mybb->input['send'];
+	} else {
+		$override_receive = $nl['override_receive'];
+		$subject = $nl['subject'];
+		$html = $nl['html'];
+		$plain = $nl['plain'];
+		$send = 0;
+	}
+
+
+	$form = new Form("index.php?module=forum-newsletter&amp;action=do_edit", "post");
+	$form_container = new FormContainer($lang->edit);
+
+	$fid = "override_receive";
+	$override_receive = $form->generate_yes_no_radio($fid, $override_receive, true, array("id" => $fid."_yes", "class" => $fid), array("id" => $fid."_no", "class" => $fid));
+	$form_container->output_row($lang->override_receive." <em>*</em>", $lang->override_receive_desc, $override_receive);
+
+	$subject = $form->generate_text_box("subject", $subject);
+	$form_container->output_row($lang->subject." <em>*</em>", $lang->subject_desc, $subject);
+
+	$plain = $form->generate_text_area("plain", $plain);
+	$form_container->output_row($lang->plain." <em>*</em>", $lang->plain_desc, $plain);
+
+	$html = $form->generate_text_area("html", $html);
+	$form_container->output_row($lang->html, $lang->html_desc, $html);
+
+	$fid = "send";
+	$send = $form->generate_yes_no_radio($fid, $send, true, array("id" => $fid."_yes", "class" => $fid), array("id" => $fid."_no", "class" => $fid));
+	$form_container->output_row($lang->send." <em>*</em>", $lang->send_desc, $send);
+
+	echo $form->generate_hidden_field("id", $id);
+	$form_container->end();
+
+	$buttons[] = $form->generate_submit_button($lang->submit);
+	$buttons[] = $form->generate_reset_button($lang->reset);
+	$form->output_submit_wrapper($buttons);
+	$form->end();
+}
+if($mybb->input['action'] == "delete") {
+	if(!strlen(trim($mybb->input['id'])))
+	{
+		flash_message($lang->newsletter_no_id, 'error');
+		admin_redirect("index.php?module=forum-newsletter");
+	}
+	$id=(int)$mybb->input['id'];
+	$db->delete_query("newsletter", "id='{$id}'");
+	flash_message($lang->newsletter_deleted, 'success');
+	admin_redirect("index.php?module=forum-newsletter");
+}
+if(!isset($mybb->input['action']) || $mybb->input['action'] == ""){
+	$page->output_header($lang->newsletter);
+	generate_tabs("list");
+
+	$table = new Table;
+
+	$table->construct_header($lang->subject);
+	$table->construct_header($lang->html);
+	$table->construct_header($lang->override_receive);
+	$table->construct_header($lang->sent);
+	$table->construct_header($lang->controls, array("class" => "align_center"));
+
+	$query = $db->simple_select("newsletter", "*", "", array("order_by"=>"created", "order_dir"=>"DESC"));
+	if($db->num_rows($query) > 0)
+	{
+		while($nl = $db->fetch_array($query))
+		{
+			if(strlen($nl['plain']) > 100)
+			    $nl['plain'] = substr($nl['plain'], 0, 100)."...";
+			    
+			if($nl['html'] != "")
+			    $html = $lang->yes;
+			else
+				$html = $lang->no;
+				
+			if($nl['override_receive'])
+			    $override_receive = $lang->yes;
+			else
+				$override_receive = $lang->no;
+
+			if($nl['sent'] != 0)
+			    $sent = $lang->yes;
+			else
+				$sent = $lang->no;
+
+			$table->construct_cell("<strong>{$nl['subject']}</strong><br /><i>{$nl['plain']}</i>");
+			$table->construct_cell($html);
+			$table->construct_cell($override_receive);
+			$table->construct_cell($sent);
+
+			$popup = new PopupMenu("nl_".$nl['id'], $lang->options);
+			if($nl['sent'] == 0) {
+				$popup->add_item($lang->send, "index.php?module=forum-newsletter&amp;action=send&amp;id={$nl['id']}");
+				$popup->add_item($lang->edit, "index.php?module=forum-newsletter&amp;action=edit&amp;id={$nl['id']}");
+			}
+			$popup->add_item($lang->delete, "index.php?module=forum-newsletter&amp;action=delete&amp;id={$nl['id']}");
+			$table->construct_cell($popup->fetch(), array("class"=>"align_center"));
+
+			$table->construct_row();
+		}
+	} else {
+		$table->construct_cell($lang->no_newsletter, array('class' => 'align_center', 'colspan' => 5));
+		$table->construct_row();
+	}
+	$table->output($lang->newsletter);
+}
 
 $page->output_footer();
+
+function generate_tabs($selected)
+{
+	global $lang, $page;
+
+	$sub_tabs = array();
+	$sub_tabs['list'] = array(
+		'title' => $lang->list,
+		'link' => "index.php?module=forum-newsletter",
+		'description' => $lang->list_desc
+	);
+	$sub_tabs['add'] = array(
+		'title' => $lang->add,
+		'link' => "index.php?module=forum-newsletter&amp;action=add",
+		'description' => $lang->add_desc
+	);
+
+	$page->output_nav_tabs($sub_tabs, $selected);
+}
 ?>
--- inc/languages/deutsch_du/admin/newsletter.lang.php
+++ inc/languages/deutsch_du/admin/newsletter.lang.php
@@ -1,4 +1,36 @@
 <?php
 $l['newsletter'] = "Newsletter";
 $l['newsletter_permissions'] = "Kann Newsletter verwalten?";
+$l['newsletter_no_id'] = "Es wurde keine ID &uuml;bermittelt";
+$l['newsletter_wrong_id'] = "Es wurde eine fehlerhafte ID &uuml;bermittelt";
+$l['newsletter_already_sent'] = "Dieser Newsletter wurde bereits verschickt";
+
+$l['list'] = "Liste";
+$l['list_desc'] = "Hier siehst du alle bisher verfassten Newsletter";
+$l['add'] = "Neuen Newsletter verfassen";
+$l['add_desc'] = "Hier kannst du einen neuen Newsletter verfassen";
+
+$l['no_newsletter'] = "Bisher wurden keine Newsletter erstellt.";
+$l['sent'] = "Newsletter bereits versendet?";
+$l['send'] = "Senden";
+
+$l['override_receive'] = "Newsletter an alle senden?";
+$l['override_receive_desc'] = "Dies &uuml;berschreibt die Einstellung der User im UCP";
+$l['override_receive_not'] = "Interner Fehler [1]";
+$l['subject'] = "Titel";
+$l['subject_desc'] = "Der Titel des Newsletters";
+$l['subject_not'] = "Bitte gebe einen Titel ein";
+$l['html'] = "HTML Nachricht";
+$l['html_desc'] = "Hier kannst du HTML zur Konvertierung nutzen<br />Falls du hier nichts eingibst, wird nur die normale Nachricht verschickt";
+$l['plain'] = "Nachricht";
+$l['plain_desc'] = "Der Standard Text deines Newsletters";
+$l['plain_not'] = "Bitte gebe eine Nachricht ein";
+$l['send_desc'] = "Soll der Newsletter direkt gesendet werden?<br />Du kannst diesen danach NICHT mehr bearbeiten";
+$l['send_not'] = "Interner Fehler [2]";
+$l['submit'] = "Speichern";
+$l['newsletter_added'] = "Newsletter gespeichert";
+
+$l['newsletter_deleted'] = "Newsletter gel&ouml;scht";
+
+$l['newsletter_edited'] = "Newsletter bearbeitet";
 ?>
