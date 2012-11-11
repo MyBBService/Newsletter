2aae8fd469d5d48ec8401ac7af0dba5c00077399
Kenny <admin@mybbservice.de>
2012-11-11 11:48:34 +0100

    deutsch_sie hinzugefügt

---
+++ inc/languages/deutsch_sie/admin/newsletter.lang.php
@@ -0,0 +1,38 @@
+<?php
+$l['newsletter'] = "Newsletter";
+$l['newsletter_permissions'] = "Kann Newsletter verwalten?";
+$l['newsletter_no_id'] = "Es wurde keine ID &uuml;bermittelt";
+$l['newsletter_wrong_id'] = "Es wurde eine fehlerhafte ID &uuml;bermittelt";
+$l['newsletter_already_sent'] = "Dieser Newsletter wurde bereits verschickt";
+
+$l['list'] = "Liste";
+$l['list_desc'] = "Hier sehen Sie alle bisher verfassten Newsletter";
+$l['add'] = "Neuen Newsletter verfassen";
+$l['add_desc'] = "Hier k&ouml;nnen Sie einen neuen Newsletter verfassen";
+
+$l['no_newsletter'] = "Bisher wurden keine Newsletter erstellt.";
+$l['sent'] = "Newsletter bereits versendet?";
+$l['send'] = "Senden";
+
+$l['override_receive'] = "Newsletter an alle senden?";
+$l['override_receive_desc'] = "Dies &uuml;berschreibt die Einstellung der User im UCP";
+$l['override_receive_not'] = "Interner Fehler [1]";
+$l['subject'] = "Titel";
+$l['subject_desc'] = "Der Titel des Newsletters";
+$l['subject_not'] = "Bitte geben Sie einen Titel ein";
+$l['html'] = "HTML Nachricht";
+$l['html_desc'] = "Hier k&ouml;nnen Sie HTML zur Konvertierung nutzen<br />Falls Sie hier nichts eingeben, wird nur die normale Nachricht verschickt";
+$l['plain'] = "Nachricht";
+$l['plain_desc'] = "Der Standard Text ihres Newsletters";
+$l['plain_not'] = "Bitte geben Sie eine Nachricht ein";
+$l['send_desc'] = "Soll der Newsletter direkt gesendet werden?<br />Sie k&ouml;nnen diesen danach NICHT mehr bearbeiten";
+$l['send_not'] = "Interner Fehler [2]";
+$l['submit'] = "Speichern";
+$l['newsletter_added'] = "Newsletter gespeichert";
+
+$l['newsletter_deleted'] = "Newsletter gel&ouml;scht";
+
+$l['newsletter_edited'] = "Newsletter bearbeitet";
+
+$l['newsletter_sent'] = "Newsletter wurde zum Versenden freigegeben";
+?>
